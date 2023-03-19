63588fa7efafe6c420c42a4b7499b8d4e10830ef
pupu <guopupu@hotmail.com>
2023-03-19 18:53:23 +0800

    temp

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-const friends = ['xp','zdm','xx'];
+const friends = ['xp', 'zdm', 'xx'];
 const newLength = friends.push('pp');
 console.log(friends);
 console.log(newLength);
@@ -12,19 +12,4 @@ const poped = friends.pop();
 console.log(poped);
 console.log(friends);
 
-const shifted =friends.shift();
-console.log(shifted);
-console.log(friends);
-console.log(friends.indexOf('pp'));
-
-console.log(friends.includes('zdm'));
-
-friends.push(27);
-console.log(friends);
-//friends.push('27');
-console.log(friends);
-console.log(friends.includes('27'));
-
-if(friends.includes('zdm')){
-    console.log('You have a friend called zdm');
-}
+rsedagdrgh
