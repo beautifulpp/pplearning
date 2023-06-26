ef3e570a01ba03225307f32aa7d4777f67232e83
pupu <guopupu@hotmail.com>
2023-06-26 16:09:25 +0800

    p59-p62

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+const calcAge = (birthYear) => 2037 - birthYear;
 
+console.log(calcAge(34));
+console.log('df');
+const x = 45;
+console.log('sf');
+console.log(`sf`);
+
+console.log('sf');
+console.log('sf');
+console.log('calc');
+console.log('cal');
+console.log('calcA');
+
+//BUG dfh
