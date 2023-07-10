26d60a4c27a3914a59932f9af17c8bdaa4cb36a2
pupu <guopupu@hotmail.com>
2023-07-10 17:08:08 +0800

    0710

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'correct number!';
+// document.querySelector('.number').textContent = '13';
+// document.querySelector('.score').textContent = '10';
+// document.querySelector('.guess').value = 23;
+// // console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.message').textContent = 'correct number';
+const score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'no munber';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'higher number!';
+    score--;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'lower number!';
+  }
+});
