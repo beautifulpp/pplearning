825bbba9415d791934e7fd5c62f4dfe5a97a10b6
pole1419 <pole1419@hotmail.com>
2023-07-24 11:03:51 +0800

    0726

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,7 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log('Button clicked');
+  //   console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,11 +17,17 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 const closeModal = function () {
-  console.log('Button closed');
+  //   console.log('Button closed');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
