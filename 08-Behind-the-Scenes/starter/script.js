a31646a154ec2da8ce7aab15872c0001de4debbf
gpp <guopupu@hotmail.com>
2023-09-10 16:36:02 +0800

    0910

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,22 +7,63 @@
 // function bar() {
 //   value = 2;
 //   foo();
+// // }
+
+// // bar();
+
+// function calcAge(birthYear) {
+//   const age = 2030 - birthYear;
+//   function printAge() {
+//     let output = `${firstName},you are ${age},born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       //BUG
+//       console.log(millenial);
+//       var millenial = true;
+
+//       //新创造内部同名常量firstName
+//       const firstName = 'PP';
+//       //修改变量output值
+//       output = 'new output';
+//       const str = `Oh,you're a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   //执行printAge
+//   printAge();
+//   return age;
+// }
+// const firstName = 'G';
+// //执行calcAge
+// calcAge(1990);
+
+// console.log(addDecl);
+// console.log(addDecl(2, 3));
+// console.log(addArrow);
+// console.log(addExpr(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
 // }
 
-// bar();
+// var addArrow = (a, b) => a + b;
 
-function calcAge(birthYear) {
-  const age = 2030 - birthYear;
-  function printAge() {
-    const output = `You are ${age},born in ${birthYear}`;
-    console.log(output);
-  }
-  printAge();
-
-  return age;
+// var addExpr = function (a, b) {
+//   return a + b;
+// };
+if (!numProducts) {
+  deleteShoppingCar();
 }
+var numProducts = 10;
 
-calcAge(1990);
-const FirstName = 'Gpp';
-
-BUG;
+function deleteShoppingCar() {
+  console.log('All products deleted!');
+}
