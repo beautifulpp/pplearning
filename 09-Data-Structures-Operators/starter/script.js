c17ace5e108e44686a68dcc2cdf76567ea9fa80c
pole1419 <pole1419@hotmail.com>
2023-09-12 23:49:03 +0800

    09122

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Data needed for a later exercise
+//Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
@@ -39,6 +39,10 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`);
+  },
 };
 
 restaurant.orderDelivery({
@@ -51,8 +55,8 @@ restaurant.orderDelivery({
   address: 'hehuanyuan38#605',
 });
 
-//Tips基础Object destructuring
-const { name, openingHours, categories } = restaurant;
+//Tips基础Object destructuring 需要对应名字
+const { openingHours, categories, name } = restaurant;
 console.log(name, openingHours, categories);
 
 //Tips改名
@@ -63,59 +67,61 @@ const {
 } = restaurant;
 console.log(restaurantName, hour, tag);
 
-const { menu = [], startMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+const { menu0 = [], startMenu: starters = [] } = restaurant;
+console.log(menu0, starters);
 
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
+let x = 111;
+let y = 999;
+const obj = { x: 23, y: 7, z: 14 };
+({ x, y } = obj);
+console.log(x, y);
 
 const {
-  fri: { open: o, close: c },
+  fri: { open: o, close: clo },
 } = restaurant.openingHours;
-console.log(o, c);
+console.log(o, clo);
 
-// const arr = [2, 3, 4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 //Tips using thr destructure assignment
-// const [x, y, z] = arr;
-// console.log(x, y, z);
-// console.log(arr);
+const [x2, y2, z2] = arr;
+console.log(x2, y2, z2);
+console.log(arr);
 //Tips 跳过某个取值用空格“a, , b ”
-// let [main, , secondary] = restaurant.categories;
-// console.log(main, secondary);
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
 
-// [secondary, main] = [main, secondary];
-// console.log(main, secondary);
-// console.log(restaurant.order(2, 0));
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
+console.log(restaurant.order(2, 0));
 
-// //Tips receive 2 return values from a function
-// const [starter, mainCourse] = restaurant.order(0, 2);
-// console.log(starter, mainCourse);
+//Tips receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(0, 2);
+console.log(starter, mainCourse);
 
-// const nested = [2, 3, [4, 5]];
-// // const [i, , j] = nested;
-// // console.log(i, j);
-// //Tips destructuring inside of destructuring
-// const [i, , [j, k]] = nested;
-// console.log(i, j, k);
+const nested = [2, 3, [4, 5]];
+// const [i, , j] = nested;
+// console.log(i, j);
+//Tips destructuring inside of destructuring
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
 
-// //Tips设置default values
-// const [p, q, r] = [8, 9];
-// console.log(p, q, r);
-// //result: 8,9,undefined
-// const [x = 1, y = 1, z = 1] = [8, 9];
-// console.log(x, y, z);
-// //result: 8,9,1
+//Tips设置default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
+//result: 8,9,undefined
+const [x3 = 1, y3 = 1, z3 = 1] = [8, 9];
+console.log(x3, y3, z3);
+//result: 8,9,1
 
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+
+const arr3 = [7, 8, 9];
+const badNewArr = [1, 2, arr3[0], arr3[1], arr3[2]];
 console.log(badNewArr);
-const newArr = [1, 2, ...arr];
+//Tips spread operator only used for write multiple values separated by commas.构建数组/传递函数的参数，仅适用于需要用‘，’分隔多个值的情况
+const newArr = [1, 2, ...arr3];
 console.log(newArr);
 // [1,2,7,8,9]
 console.log(...newArr);
@@ -123,3 +129,41 @@ console.log(...newArr);
 
 const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+const menu = [...restaurant.startMenu, ...restaurant.mainMenu]
+console.log(menu)
+
+//Tips iterables: arrays,strings,maps,sets. Not objects before ES2018
+const str = 'Jonas';
+const letters = [...str, '', 'P.'];
+console.log(letters);
+
+// const ingredients = [prompt("Let's make pasta! Infredient 1?"), prompt("Infredient 2?"), prompt("Infredient 3?")];
+// console.log(ingredients);
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+//objects
+const newRestaurant = { foundedin: 1991, ...restaurant, founder: 'gpp' }
+console.log(newRestaurant);
+const restaurantCopy = { ...newRestaurant };
+restaurantCopy.name = 'ristorante roma';
+console.log(newRestaurant.name, restaurantCopy.name);
+
+//Tips spread,because on right side of "="
+const arr4 = [1, 2, ...[3, 4]];
+console.log(arr4);
+// [1,2,3,4]
+
+//Tips rest,because on left side of "="
+const [a4, b4, ...others] = [1, 2, 3, 4];
+console.log(a4, b4, others);
+// 1 2 [3,4]
+
+const [Pizza, , Risotto, Focaccia, ...others2] = [...restaurant.mainMenu, ...restaurant.startMenu];
+console.log(Pizza, Risotto, Focaccia, others2);
+
+//objects  需要对应名字
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
