b4ceb19e6ed0c5cb756d16b80726837fda7ad3c0
pole <pole1419@hotmail.com>
2023-09-17 23:04:29 +0800

    chore: stash

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -133,214 +133,218 @@ for (const [day, { open, close }] of entries) {
   console.log(`on the ${day}, we open at ${open} and close at ${close}`);
 }
 
-// restaurant.orderDelivery({
-//   time: '12:00',
-//   address: 'changheli380',
-//   startIndex: 2,
-//   mainIndex: 1,
-// });
-// restaurant.orderDelivery({
-//   address: 'hehuanyuan38#605',
-// });
+restaurant.orderDelivery({
+  time: '12:00',
+  address: 'changheli380',
+  startIndex: 2,
+  mainIndex: 1,
+});
+restaurant.orderDelivery({
+  address: 'hehuanyuan38#605',
+});
 
-// //Tips基础Object destructuring 需要对应名字
-// const { name, openingHours, categories } = restaurant;
-// console.log(name, openingHours, categories);
+//Tips基础Object destructuring 需要对应名字
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 
-// //Tips改名
-// const {
-//   name: restaurantName,
-//   openingHours: hour,
-//   categories: tag,
-// } = restaurant;
-// console.log(restaurantName, hour, tag);
+//Tips改名
+const {
+  name: restaurantName,
+  openingHours: hour,
+  categories: tag,
+} = restaurant;
+console.log(restaurantName, hour, tag);
 
-// const { menu0 = [], startMenu: starters = [] } = restaurant;
-// console.log(menu0, starters); //Array[0];Array[4]
+const { menu0 = [], startMenu: starters = [] } = restaurant;
+console.log(menu0, starters); //Array[0];Array[4]
 
-// let x = 111;
-// let y = 999;
-// const obj = { x: 23, y: 7, z: 14 };
-// ({ x, y } = obj);
-// console.log(x, y); // 23  7
+let x = 111;
+let y = 999;
+const obj = { x: 23, y: 7, z: 14 };
+({ x, y } = obj);
+console.log(x, y); // 23  7
 
-// const {
-//   fri: { open: o, close: clo },
-// } = restaurant.openingHours;
-// console.log(o, clo);
+const {
+  fri: { open: o, close: clo },
+} = restaurant.openingHours;
+console.log(o, clo);
 
-// const arr = [2, 3, 4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
-// //Tips using the Destructure Assignment
-// const [x2, y2, z2] = arr;
-// console.log(x2, y2, z2);
-// console.log(arr);
+//Tips using the Destructure Assignment
+const [x2, y2, z2] = arr;
+console.log(x2, y2, z2);
+console.log(arr);
 
-// //Tips 跳过某个取值用空格“a, , b ”
-// let [main, , secondary] = restaurant.categories;
-// console.log(main, secondary);
+//Tips 跳过某个取值用空格“a, , b ”
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
 
-// [secondary, main] = [main, secondary];
-// console.log(main, secondary);
-// console.log(restaurant.order(2, 0));
+[secondary, main] = [main, secondary];
+console.log(main, secondary);
+console.log(restaurant.order(2, 0));
 
-// //Tips receive 2 return values from a function
-// const [starter, mainCourse] = restaurant.order(0, 2);
-// console.log(starter, mainCourse);
+//Tips receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(0, 2);
+console.log(starter, mainCourse);
 
-// const nested = [2, 3, [4, 5]];
-// // const [i, , j] = nested;
-// // console.log(i, j);
-// //Tips destructuring inside of destructuring
-// const [i, , [j, k]] = nested;
-// console.log(i, j, k);
+const nested = [2, 3, [4, 5]];
+// const [i, , j] = nested;
+// console.log(i, j);
+//Tips destructuring inside of destructuring
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
 
-// //Tips设置default values
-// const [p, q, r] = [8, 9];
-// console.log(p, q, r);
-// //result: 8,9,undefined
-// const [x3 = 1, y3 = 1, z3 = 1] = [8, 9];
-// console.log(x3, y3, z3);
-// //result: 8,9,1
+//Tips设置default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
+//result: 8,9,undefined
+const [x3 = 1, y3 = 1, z3 = 1] = [8, 9];
+console.log(x3, y3, z3);
+//result: 8,9,1
 
-// const arr3 = [7, 8, 9];
-// const badNewArr = [1, 2, arr3[0], arr3[1], arr3[2]];
-// console.log(badNewArr);
-// //Tips spread operator only used for write multiple values separated by commas.构建数组/传递函数的参数，仅适用于需要用‘，’分隔多个值的情况
-// const newArr = [1, 2, ...arr3];
-// console.log(newArr);
-// // [1,2,7,8,9]
-// console.log(...newArr);
-// // 1,2,7,8,9
+const arr3 = [7, 8, 9];
+const badNewArr = [1, 2, arr3[0], arr3[1], arr3[2]];
+console.log(badNewArr);
+//Tips spread operator only used for write multiple values separated by commas.构建数组/传递函数的参数，仅适用于需要用‘，’分隔多个值的情况
+const newArr = [1, 2, ...arr3];
+console.log(newArr);
+// [1,2,7,8,9]
+console.log(...newArr);
+// 1,2,7,8,9
 
-// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-// console.log(newMenu);
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
 
-// const mainMenuCopy = [...restaurant.mainMenu];
-// const menu = [...restaurant.startMenu, ...restaurant.mainMenu];
-// console.log(menu);
+const mainMenuCopy = [...restaurant.mainMenu];
+const menu = [...restaurant.startMenu, ...restaurant.mainMenu];
+console.log(menu);
 
-// //Tips the 'for-of' loop
-// for (const item of menu) {
-//   console.log(item);
-// }
+//Tips the 'for-of' loop
+for (const item of menu) {
+  console.log(item);
+}
 
-// //Tips iterables: arrays,strings,maps,sets. Not objects before ES2018
-// const str = 'Jonas';
-// const letters = [...str, '', 'P.'];
-// console.log(letters);
+//Tips iterables: arrays,strings,maps,sets. Not objects before ES2018
+const str = 'Jonas';
+const letters = [...str, '', 'P.'];
+console.log(letters);
 
-// const ingredients = [prompt("Let's make pasta! Infredient 1?"), prompt("Infredient 2?"), prompt("Infredient 3?")];
+// const ingredients = [
+//   prompt("Let's make pasta! Infredient 1?"),
+//   prompt('Infredient 2?'),
+//   prompt('Infredient 3?'),
+// ];
 // console.log(ingredients);
 // restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
 // restaurant.orderPasta(...ingredients);
 
-//objects
-// const newRestaurant = { foundedin: 1991, ...restaurant, founder: 'gpp' };
-// console.log(newRestaurant);
-// const restaurantCopy = { ...newRestaurant };
-// restaurantCopy.name = 'ristorante roma';
-// console.log(newRestaurant.name, restaurantCopy.name);
+//objects;
+const newRestaurant = { foundedin: 1991, ...restaurant, founder: 'gpp' };
+console.log(newRestaurant);
+const restaurantCopy = { ...newRestaurant };
+restaurantCopy.name = 'ristorante roma';
+console.log(newRestaurant.name, restaurantCopy.name);
 
-// // 1) Destructuring
-// //Tips spread,because on right side of "="
-// const arr4 = [1, 2, ...[3, 4]];
-// console.log(arr4);
-// // [1,2,3,4]
+// 1) Destructuring
+//Tips spread,because on right side of "="
+const arr4 = [1, 2, ...[3, 4]];
+console.log(arr4);
+// [1,2,3,4]
 
-// //Tips rest,because on left side of "="
-// const [a4, b4, ...others] = [1, 2, 3, 4];
-// console.log(a4, b4, others);
-// // 1 2 [3,4]
+//Tips rest,because on left side of "="
+const [a4, b4, ...others] = [1, 2, 3, 4];
+console.log(a4, b4, others);
+// 1 2 [3,4]
 
-// const [Pizza, , Risotto, Focaccia, ...others2] = [
-//   ...restaurant.mainMenu,
-//   ...restaurant.startMenu,
-// ];
-// console.log(Pizza, Risotto, Focaccia, others2);
+const [Pizza, , Risotto, Focaccia, ...others2] = [
+  ...restaurant.mainMenu,
+  ...restaurant.startMenu,
+];
+console.log(Pizza, Risotto, Focaccia, others2);
 
-// //objects  需要对应名字
-// const { sat, ...weekdays } = restaurant.openingHours;
-// console.log(sat, weekdays);
+//objects  需要对应名字
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
 
-// // 2) Functions
-// const add = function (...numbers) {
-//   let sum = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     sum += numbers[i];
-//   }
-//   return sum;
-// };
-// console.log(add(1, 2, 3, 4, 15));
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
+console.log(add(1, 2, 3, 4, 15));
 
-// const t = [23, 5, 7];
-// console.log(add(...t));
+const t = [23, 5, 7];
+console.log(add(...t));
 
-// restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
 
-// console.log('-------OR-------');
-// //Tips 3 property of logical operators( &&  || ):
-// // 1)  use any data type, retrun any data type,short-circuiting
-// console.log(3 || 'pp'); // 3
-// console.log('pp' || 3); // 'pp'
-// console.log('' || 'pp'); //'pp'
-// console.log(true || 3); //true
-// console.log(undefined || null); //null
-// console.log(undefined || 0 || '' || 'pp' || 23 || null); //'pp'
+console.log('-------OR-------');
+//Tips 3 property of logical operators( &&  || ):
+// 1)  use any data type, retrun any data type,short-circuiting
+console.log(3 || 'pp'); // 3
+console.log('pp' || 3); // 'pp'
+console.log('' || 'pp'); //'pp'
+console.log(true || 3); //true
+console.log(undefined || null); //null
+console.log(undefined || 0 || '' || 'pp' || 23 || null); //'pp'
 
-// restaurant.numGuests = 23;
-// const guestes1 = restaurant.numGuests ? restaurant.numGuests : 10;
-// console.log(guestes1);
+restaurant.numGuests = 23;
+const guestes1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guestes1);
 
-// const guests2 = restaurant.numGuests || 10;
-// console.log(guests2);
-// // summarize 'OR' return the first truthy value of all the operands, or simply the last value if all of them are falsy
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+// summarize 'OR' return the first truthy value of all the operands, or simply the last value if all of them are falsy
 
-// console.log('-------AND-------');
-// console.log(0 && 'pp'); //0
-// console.log(7 && 'pp'); //'pp'
-// console.log('pp' && 23 && null && 'g'); //null
-// //practical example
-// if (restaurant.orderPizza) {
-//   restaurant.orderPizza('mushroom', 'spinach');
-// }
+console.log('-------AND-------');
+console.log(0 && 'pp'); //0
+console.log(7 && 'pp'); //'pp'
+console.log('pp' && 23 && null && 'g'); //null
+//practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushroom', 'spinach');
+}
 
-// restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
-// //summarize 'AND' return the first falsy value or the last value if all of them are truthy
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+//summarize 'AND' return the first falsy value or the last value if all of them are truthy
 
-// restaurant.numGuests = 0;
-// const guests3 = restaurant.numGuests || 10;
-// console.log(guests3);
+restaurant.numGuests = 0;
+const guests3 = restaurant.numGuests || 10;
+console.log(guests3);
 
-// //Tips the Nullish Coalescing Operator : null and undefined (NOT 0 or '')
-// const guestsCorrect = restaurant.numGuests ?? 10;
-// console.log(guestsCorrect);
+//Tips the Nullish Coalescing Operator : null and undefined (NOT 0 or '')
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
 
-// const rest1 = {
-//   name: 'Capri',
-//   numGuests: 0,
-// };
-// const rest2 = {
-//   name: 'La Piazza',
-//   owner: 'pp',
-// };
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'pp',
+};
 
-// // rest1.numGuests = rest1.numGuests || 10;
-// // rest2.numGuests = rest2.numGuests || 10;
-// // console.log(rest1, rest2);
-
-// rest1.numGuests ??= 10;
-// rest2.numGuests ||= 10;
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
 // console.log(rest1, rest2);
 
-// // rest1.owner = rest1.owner && 'gpp ';
-// // rest2.owner = rest2.owner && 'gpp ';
-// rest1.owner &&= 'GPP';
-// rest2.owner &&= 'GPP';
-// console.log(rest1, rest2);
+rest1.numGuests ??= 10;
+rest2.numGuests ||= 10;
+console.log(rest1, rest2);
+
+// rest1.owner = rest1.owner && 'gpp ';
+// rest2.owner = rest2.owner && 'gpp ';
+rest1.owner &&= 'GPP';
+rest2.owner &&= 'GPP';
+console.log(rest1, rest2);
 
 // Coding Challenge #1
 
@@ -538,27 +542,17 @@ for (const player of game.scored) {
 console.log(scorers);
 
 //Tips Set
-// const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto']);
-// const ordersSet2 = new Set('Pasta', 'Pizza', 'Pizza', 'Risotto');
-// console.log(ordersSet2);
-// console.log(ordersSet, ordersSet2);
-// console.log(new Set('ordersSet'));
-// console.log(new Set(['ordersSet']));
-// console.log(ordersSet.size);
-// console.log(ordersSet.has('Pizza'));
-// console.log(ordersSet.has('Bread'));
-// ordersSet.add('Garlic Bread');
-// ordersSet.add('Garlic Bread');
-// ordersSet.delete('Risotto');
-// // ordersSet.cle ar();
-// console.log(ordersSet);
-// for (const order of ordersSet) {
-//   console.log(order);
-// }
+const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto']);
+console.log(ordersSet);
+console.log(new Set('ordersSet'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
 
-const staff = ['Pasta', 'Pizza', 'Pizza', 'Risotto'];
-const staffUnique = new Set([staff]);
-staffUnique.add('bbb');
-console.log('------', staffUnique);
-// const staffUnique2 = new Set(staff);
-// console.log(staffUnique, staffUnique2);
+//Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = new Set(staff);
+console.log(2222);
