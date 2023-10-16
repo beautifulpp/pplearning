3ef48655dbf610f96e89e350e7458b8d051ea069
pole <pole1419@hotmail.com>
2023-10-17 00:22:10 +0800

    20231016night

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -219,22 +219,33 @@ const poll = {
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
 
-  registerNewAnswer: function () {
-    prompt(`What is your favourite programming language?
-    0: JavaScript
-    1: Python
-    2: Rust
-    3: C++
-    (Write option number)`);
-    return displayResults;
+  registerNewAnswer() {
+    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+    // console.log(answer);
+    // register answer
+    typeof answer === 'number' && answer < this.options.length && this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string')
   },
-  displayResults: function (type) {
-    if (typeof type === Array) {
-      console.log(type);
-    } else if (typeof type === String) {
-      console.log(`Poll results are ` + type);
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
     }
   },
 };
-const btnPoll = document.querySelector('.poll');
-btnPoll.addEventListener('click', poll.registerNewAnswer);
+
+// poll.registerNewAnswer();
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+// poll.displayResults();
+
+poll.displayResults.call({ answers: [5, 2, 3] })
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string')
+
+// const btnPoll = document.querySelector('.poll');
+// btnPoll.addEventListener('click', poll.registerNewAnswer);
